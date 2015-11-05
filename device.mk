23d3fed00e4dd8689a231d9bba452fdba970f3ce
Legrand Benjamin <android@legrand.ws>
2015-11-05 21:45:07 +0100

    Add ffmpeg stagefright plugins + codecs

--- device.mk
+++ device.mk
@@ -134,6 +134,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
+PRODUCT_PACKAGES += \
+    libffmpeg_omx \
+    libffmpeg_utils \
+    media_codecs_ffmpeg.xml
+
 # Misc
 PRODUCT_PACKAGES += \
     libbson \
